b54b12df7c8253abac1325814e47cb5443b27510
dsalex1 <dsalex1@ymail.com>
2022-08-09 18:07:47 +0200

    initial commit/scaffholding

---
+++ src/components/CookieBanner.vue
@@ -0,0 +1,20 @@
+<template>
+  <vue-cookie-comply
+    :preferences="[
+      {
+        title: 'Required Cookies',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sit amet malesuada libero.',
+        items: [{ label: 'Active', value: 'performance', isRequired: true }],
+      },
+      {
+        title: 'Statistical and Comfort Cookies',
+        description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sit amet malesuada libero.',
+        items: [{ label: 'Active', value: 'statistics' }],
+      },
+    ]"
+    @on-accept-all-cookies="() => {}"
+    @on-save-cookie-preferences="() => {}"
+  />
+</template>
